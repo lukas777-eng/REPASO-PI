73c5be6c1b4d91010149f84de7dc4e73478cd52f
lukas777-eng <lucastomascasco@gmail.com>
2022-05-03 23:15:33 -0300

    add model Character

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -4,6 +4,31 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("character", {
-
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,  //me genera automáticamente un UUIDV4 que es un datatype de Sequelize
+      allowNull: false,                 //no se permite que este campo esté vacío, o sea, es un campo requerido
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    species: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    origin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
 };
