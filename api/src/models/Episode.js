64e2f8a9a81c286dbeba3f89fa06a2f51dd11e34
lukas777-eng <lucastomascasco@gmail.com>
2022-05-03 23:16:17 -0300

    add model Episode

--- api/src/models/Episode.js
+++ api/src/models/Episode.js
@@ -4,6 +4,15 @@ const { DataTypes } = require('sequelize')
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('episode', {
-
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,  //me genera automáticamente un UUIDV4 que es un datatype de Sequelize
+      allowNull: false,                 //no se permite que este campo esté vacío, o sea, es un campo requerido
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   })
 }
